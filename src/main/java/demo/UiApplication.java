20d6165cf10a33f269c5392473362952c85ba545
Javier Badaracco <javierbada@hotmail.com>
2015-04-20 02:34:26 -0300

    first commit

---
+++ src/main/java/demo/UiApplication.java
@@ -0,0 +1,66 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+@SpringBootApplication
+@RestController
+@EnableRedisHttpSession
+public class UiApplication {
+
+	@RequestMapping("/token")
+	@ResponseBody
+	public Map<String, String> token(HttpSession session) {
+		return Collections.singletonMap("token", session.getId());
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiApplication.class, args);
+	}
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends
+			WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.httpBasic()
+					.and()
+					.authorizeRequests()
+					.antMatchers("/index.html", "/home.html", "/login.html",
+							"/").permitAll().anyRequest().authenticated().and()
+					.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+					.csrf().csrfTokenRepository(csrfTokenRepository()).and()
+					.logout();
+		}
+
+		private CsrfTokenRepository csrfTokenRepository() {
+			HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+			repository.setHeaderName("X-XSRF-TOKEN");
+			return repository;
+		}
+	}
+}
